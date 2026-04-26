27d3b2c5776eabc75533ecbea1ab24c0406a532f
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2026-04-26 10:49:59 +0200

    minor coliding function refactor

--- src/layers/MainLayer.js
+++ src/layers/MainLayer.js
@@ -18,6 +18,7 @@ class MainLayer {
 
     update() {
         this.bodies.forEach(b => b.update());
+        this.fuseCollidingBodies();
     }
 
     draw() {
@@ -25,60 +26,56 @@ class MainLayer {
         ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
         this.bodies.forEach(b => { 
-            b.draw(
-                this.bodies.filter(otherBody => otherBody != b)
-                .map(otherBody => otherBody.traslateCoordinate(this.executionStart, b))
-            );
-        });
-        this.fuseCollidingBodies();
-    }
-
-    fuseCollidingBodies() { // TODO change to O(n)
-        this.bodies.filter(body => body.x > canvasWidth*2 || body.x < -canvasWidth*2).forEach(body => this.bodies.splice(this.bodies.indexOf(body), 1));
-        this.bodies.filter(body => body.y > canvasHeight*2 || body.y < -canvasHeight*2).forEach(body => this.bodies.splice(this.bodies.indexOf(body), 1));
-
-        this.bodies.filter(body => body.traceCache.length > 0).forEach(body => {
-            this.bodies.filter(secondBody => 
-                // Math.abs(body.x - secondBody.x) < Utils.getRadiusForBody(body) &&
-                // Math.abs(body.y - secondBody.y) < Utils.getRadiusForBody(body) &&
-                secondBody.traceCache.length > 0 &&
-                secondBody != body)
-                .some(secondBody => {    
-                    let firstBodyVisualPosition = body.traceCache[body.traceCache.length-1];
-                    let firstBodyRadius = Utils.getRadiusForBody(body);
-                    let secondBodyVisualPosition = secondBody.traceCache[secondBody.traceCache.length-1];
-                    let secondBodyRadius = Utils.getRadiusForBody(secondBody);
-                    let distance = Math.sqrt((firstBodyVisualPosition.x - secondBodyVisualPosition.x)**2 + (firstBodyVisualPosition.y - secondBodyVisualPosition.y)**2);
-                    if (distance < firstBodyRadius || distance < secondBodyRadius) {
-                        let biggerBody = body.mass > secondBody.mass ? body : secondBody;
-                        this.bodies.splice(this.bodies.indexOf(body), 1);
-                        this.bodies.splice(this.bodies.indexOf(secondBody), 1);
-    
-                        let totalMass = body.mass + secondBody.mass;
-                        let fusedBody = new CelestialBody(
-    
-                            (body.x*body.mass+secondBody.x*secondBody.mass)/totalMass, (body.y*body.mass+secondBody.y*secondBody.mass)/totalMass,
-                            { 
-                                vx: (body.speed.vx*body.mass + secondBody.speed.vx*secondBody.mass)/totalMass,
-                                vy: (body.speed.vy*body.mass + secondBody.speed.vy*secondBody.mass)/totalMass
-                            }, 
-                            body.mass + secondBody.mass
-                        );
-                        fusedBody.color = biggerBody.color;
-                        this.bodies.push(fusedBody);
-                        //return; // this.fuseCollidingBodies();
-                        //i = 0;
-                        return true;
-                    }
-                });
+            const traslatedCoordinates = this.bodies.filter(otherBody => otherBody != b).map(otherBody => otherBody.traslateCoordinate(this.executionStart, b));
+            b.draw(traslatedCoordinates);
         });
     }
 
+    
     addNewRandomBody() {
         this.bodies.push(new CelestialBody(canvasWidth*0.1 + canvasWidth*Math.random()*0.8, canvasHeight*0.1 + canvasHeight*Math.random()*0.8, { 
             vx: 400*Math.random()*(Math.random() > 0.5 ? -1 : 1), 
             vy: 400*Math.random()*(Math.random() > 0.5 ? -1 : 1) 
         }, 3000*Math.random()+100));
+    }
+
+    fuseCollidingBodies() {
+        this.bodies.filter(body => body.x > canvasWidth*2 || body.x < -canvasWidth*2).forEach(body => this.bodies.splice(this.bodies.indexOf(body), 1));
+        this.bodies.filter(body => body.y > canvasHeight*2 || body.y < -canvasHeight*2).forEach(body => this.bodies.splice(this.bodies.indexOf(body), 1));
+        const getBodiesWithRealPosition = () => this.bodies.filter(body => body.traceCache.length > 0);
+
+        getBodiesWithRealPosition().forEach(body => {
+            getBodiesWithRealPosition().filter(secondBody => 
+                Math.abs(body.x - secondBody.x) < Utils.getRadiusForBody(body) && Math.abs(body.y - secondBody.y) < Utils.getRadiusForBody(body) && secondBody != body)
+            .some(secondBody => this.fuseBodyPair(body, secondBody));
+        });
+    }
+
+    fuseBodyPair(body, secondBody) {
+        let firstBodyVisualPosition = body.traceCache[body.traceCache.length-1];
+        let firstBodyRadius = Utils.getRadiusForBody(body);
+        let secondBodyVisualPosition = secondBody.traceCache[secondBody.traceCache.length-1];
+        let secondBodyRadius = Utils.getRadiusForBody(secondBody);
+                    
+        let distance = Math.sqrt((firstBodyVisualPosition.x - secondBodyVisualPosition.x)**2 + (firstBodyVisualPosition.y - secondBodyVisualPosition.y)**2);
+        if (distance < firstBodyRadius || distance < secondBodyRadius) {
+            let biggerBody = body.mass > secondBody.mass ? body : secondBody;
+            this.bodies.splice(this.bodies.indexOf(body), 1);
+            this.bodies.splice(this.bodies.indexOf(secondBody), 1);
+    
+            let totalMass = body.mass + secondBody.mass;
+            let fusedBody = new CelestialBody(
+                (body.x*body.mass+secondBody.x*secondBody.mass)/totalMass, (body.y*body.mass+secondBody.y*secondBody.mass)/totalMass,
+                { 
+                    vx: (body.speed.vx*body.mass + secondBody.speed.vx*secondBody.mass)/totalMass,
+                    vy: (body.speed.vy*body.mass + secondBody.speed.vy*secondBody.mass)/totalMass
+                }, 
+                totalMass
+            );
+            fusedBody.color = biggerBody.color;
+            this.bodies.push(fusedBody);
+            return true;
+        }
     }
 
 }
